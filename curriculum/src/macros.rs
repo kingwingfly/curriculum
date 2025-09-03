6bbfe401e6d84c188dd6c199584e77da184a9f61
Louis <836250617@qq.com>
2025-09-04 00:33:18 +0800

    better course macro to create course with multi interval periods

--- curriculum/src/macros.rs
+++ curriculum/src/macros.rs
@@ -5,36 +5,41 @@ pub macro course(
     $name: literal,
     $place: literal,
     ($($tutor_name: literal),*$(,)?),
-    (
+    $(
         (
-            $year: literal,
-            $month: literal,
-            $day: literal,
-            $hour: literal,
-            $minute: literal,
-            $second: literal
-        ),
-        $num: literal,
-        $interval: literal
-    ),
+            (
+                $year: literal,
+                $month: literal,
+                $day: literal,
+                $hour: literal,
+                $minute: literal,
+                $second: literal
+            ),
+            $num: literal,
+            $interval: literal
+        )
+    ),+,
     $duration: literal
 ) {
     $crate::course::Course::builder()
         .name($name)
         .place($place)
         .tutors([$($crate::tutor::Tutor::builder().name($tutor_name).build()),*])
-        .periods({
-            let date = $crate::NaiveDate::from_ymd_opt($year, $month, $day).unwrap();
-            (0..$num)
-                .filter_map(|i| date.checked_add_days($crate::Days::new(i * $interval)))
-                .filter_map(|d| d.and_hms_opt($hour, $minute, $second))
-                .map(|ts| {
-                    $crate::Period::builder()
-                        .start(ts)
-                        .duration($crate::TimeDelta::minutes($duration))
-                        .build()
-                })
-                .collect::<Vec<_>>()
-        })
+        .periods(
+            [].into_iter()
+                $(.chain({
+                    let date = $crate::NaiveDate::from_ymd_opt($year, $month, $day).unwrap();
+                    (0..$num)
+                        .filter_map(|i| date.checked_add_days($crate::Days::new(i * $interval)))
+                        .filter_map(|d| d.and_hms_opt($hour, $minute, $second))
+                        .map(|ts| {
+                            $crate::Period::builder()
+                                .start(ts)
+                                .duration($crate::TimeDelta::minutes($duration))
+                                .build()
+                        })
+                        .collect::<Vec<_>>()
+                }))+
+        )
         .build()
 }
